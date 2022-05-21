fb0559ada5aadc0f095120f46065755333610a25
taotao54321 <taotao54321@users.noreply.github.com>
2022-05-21 21:26:24 +0000

    deploy: 9a7994651649820c1f6c212fe78144f10c5a9772

---
+++ pkg/README.md
@@ -0,0 +1,7 @@
+# Shisen-Sho for web
+
+## Credits
+
+* "[riichi-mahjong-tiles](https://github.com/FluffyStuff/riichi-mahjong-tiles)"
+  by [@FluffyStuff](https://github.com/FluffyStuff) is licensed under
+  [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
---
+++ pkg/package_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf3efb0d780a25fd1(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2caf99d8b12a541c(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h961b4968de5cf780(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h4c0bfc8b2e84a830(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke3_mut__h05cbaf73c2551be8(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbindgen_start(): void;
